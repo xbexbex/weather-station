1d4e059cbe74b43a8167827a71f2345b5952841c
xbexbex <petja.valkama@gmail.com>
2018-01-25 17:25:11 +0200

    ADD adding readings

--- src/app/components/observation-point/observation-point.component.ts
+++ src/app/components/observation-point/observation-point.component.ts
@@ -11,12 +11,31 @@ export class ObservationPointComponent implements OnInit {
 
   @Input() observationPointKey: string;
   name: string;
-
+  temperatures: {};
+  temperatureInput: number;
+  dateInput: string;
+  timeInput: string;
   constructor(public db: AngularFireLiteDatabase) { }
 
   ngOnInit() {
     this.db.read('observation-points/' + this.observationPointKey).subscribe((data) => {
       this.name = data[1]; //returns the data in a weird array format instead of proper json, hopefully to be fixed later
+      this.temperatures = data[2][0];
     });
+  }
+
+  sendTemperature(temperatureInput: number, timeInput: string, dateInput: string) {
+    if (this.validateTemperature(temperatureInput) && this.validateDateTime(dateInput, timeInput)) {
+      const data = {temperature: temperatureInput, date: dateInput, time: timeInput};
+      this.db.push('observation-points/' + this.observationPointKey + '/readings', data);
+    }
+  }
+
+  validateTemperature(temperature: number) {
+    return true;
+  }
+
+  validateDateTime(date: string, time: string) {
+    return true;
   }
 }
