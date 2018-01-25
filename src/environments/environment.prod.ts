7e9dd83483c7c8d009352291160645dbb1e31a0b
xbexbex <petja.valkama@gmail.com>
2018-01-25 13:27:06 +0200

    ADD firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC0mlACsMyvumPUeEUC44dom7DAMwAZEAQ',
+    authDomain: 'weather-data-ronktor.firebaseapp.com',
+    databaseURL: 'https://weather-data-ronktor.firebaseio.com',
+    projectId: 'weather-data-ronktor',
+    storageBucket: 'weather-data-ronktor.appspot.com',
+    messagingSenderId: '1059518516058'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC0mlACsMyvumPUeEUC44dom7DAMwAZEAQ',
+    authDomain: 'weather-data-ronktor.firebaseapp.com',
+    databaseURL: 'https://weather-data-ronktor.firebaseio.com',
+    projectId: 'weather-data-ronktor',
+    storageBucket: 'weather-data-ronktor.appspot.com',
+    messagingSenderId: '1059518516058'
+  }
 };
