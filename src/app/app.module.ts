fd88865e7f2d22ed433832f5d0751fdfdf34267d
xbexbex <petja.valkama@gmail.com>
2018-01-25 13:31:33 +0200

    ADD material 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
+import {
+  MatButtonModule,
+  MatInputModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +18,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
