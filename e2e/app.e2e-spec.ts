f1e3f68453e035dccf71847dcbda6b2ec08fea29
xbexbex <petja.valkama@gmail.com>
2018-01-29 17:29:57 +0200

    ADD travis -> heroku pipeline

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('weather-station App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('');
   });
 });
