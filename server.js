15a8f74d7dbdb1a1fc2df8ee59ae3a36f8b1e3f9
xbexbex <petja.valkama@gmail.com>
2018-02-04 19:06:27 +0200

    testing heroku

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 let port = process.env.PORT || 8080;
-app.listen(port, function() {
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, '0.0.0.0', function() {
   console.log("Running on port " + port);
 });
