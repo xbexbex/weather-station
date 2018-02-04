becbe2066e978ddd0d751df1ecb9760756a5748f
xbexbex <petja.valkama@gmail.com>
2018-02-04 19:21:07 +0200

    testing heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 let port = process.env.PORT || 8080;
