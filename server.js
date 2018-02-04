d1d714046bbb8e328ddb0e1b8d68b18105758c48
xbexbex <petja.valkama@gmail.com>
2018-02-04 20:09:12 +0200

    ADD favicon

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + "/dist"));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 let port = process.env.PORT || 8080;
